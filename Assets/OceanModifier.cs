e314d3706f93629239c1b51e01dc9843178263d8
YANG WANG <yang.wang.dec@gmail.com>
2016-04-03 17:25:35 -0400

    incorporated wave speed

--- Assets/OceanModifier.cs
+++ Assets/OceanModifier.cs
@@ -5,24 +5,28 @@ public class OceanModifier : MonoBehaviour {
 
 	float eegReading;
 	public bool controllingWaves = true;
+
+
 	//normal
 	float minOceanScale = 4.0f;
-	float normalWaveSpeed = 0.7f;
+	float minWaveSpeed = 0.3f;
+	float minVol = 0.05f;
 
 	//max
-	float maxWaveSpeed = 3.0f;
+	float maxWaveSpeed = 1.65f;
 	float maxOceanScale = 12.5f;
-	float maxVol = 1.5f;
+	float maxVol = 1.0f;
 
 
 	float incrementArg = 0.01f;
+	float waveSpeedIncrementArg = 0.0001f;
 
 	//current
 	float oceanScale;//1-10
 	float waveSpeed; //0-3
 
 
-	float vol = 1.0f;
+	public float vol = 1.0f;
 
 
 	public Ocean myOcean;
@@ -30,23 +34,25 @@ public class OceanModifier : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		oceanScale = minOceanScale;
-		waveSpeed = normalWaveSpeed;
+		waveSpeed = minWaveSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		eegReading = GameObject.Find("osc").GetComponent<oscReceive>().mellowValue;
 
-		oceanScale = (CustomFunc.Map(eegReading,1,0,minOceanScale,maxOceanScale) - oceanScale) * incrementArg + oceanScale;
-		waveSpeed = CustomFunc.Map(eegReading,1,0,normalWaveSpeed,maxWaveSpeed);
-		vol = CustomFunc.Map (oceanScale, minOceanScale, maxOceanScale, 0.2f, 1.0f);
+		oceanScale = ( CustomFunc.Map(eegReading,1,0,minOceanScale,maxOceanScale) - oceanScale ) * incrementArg + oceanScale;
+		waveSpeed =  ( CustomFunc.Map(eegReading,1,0,minWaveSpeed,maxWaveSpeed) - waveSpeed )  * waveSpeedIncrementArg + waveSpeed;
+		vol = CustomFunc.Map (oceanScale, minOceanScale, maxOceanScale, minVol, maxVol);
 
 		AudioListener.volume = vol;
+
 		if(oceanScale>maxOceanScale)oceanScale = maxOceanScale;
 		if(waveSpeed>maxWaveSpeed)waveSpeed = maxWaveSpeed;
 
 		if(controllingWaves){
 			myOcean.scale = oceanScale;
+			myOcean.speed = waveSpeed;
 		}
 
 
