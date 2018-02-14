16b48c0a5d91399c63202636f5112556951029d3
YANG WANG <yang.wang.dec@gmail.com>
2018-02-14 12:10:21 -0500

    serial working

---
+++ Assets/PIRSensor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+public class PIRSensor : MonoBehaviour
+{
+
+    SerialPort stream = new SerialPort("/dev/cu.usbmodem1421", 9600);
+
+    // Use this for initialization
+    void Start()
+    {
+        stream.ReadTimeout = 10;
+        stream.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stream.IsOpen)
+        {
+            try
+            {
+                string input = stream.ReadLine();
+                Debug.Log(input);
+            }
+            catch (System.Exception e)
+            {
+             
+            }
+
+
+        }
+
+
+    }
+}
